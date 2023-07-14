a7f525c00e6060e71c64cec5b26c1ffa15b3421e
Vitali Asheichyk <vitali.asheichyk@jetbrains.com>
2023-07-14 17:34:16 +0200

    add comments

--- sample-script.js
+++ sample-script.js
@@ -6,13 +6,16 @@ const filepath = 'sample-output.txt';
 const binaryFilepath = 'sample-image.jpg';
 
 export default function () {
+    // Write/append string to file
     file.writeString(filepath, 'New file. First line.\n');
     file.appendString(filepath, `Second line. VU: ${__VU}  -  ITER: ${__ITER}`);
 
+    // Remove rows from text file/clear file content/delete file
     file.removeRowsBetweenValues(filepath, 2, 2);
     file.clearFile(filepath);
     file.deleteFile(filepath);
-  
+
+    // Write binary file
     let response = http.get("https://upload.wikimedia.org/wikipedia/commons/3/3f/JPEG_example_flower.jpg", {
         responseType: "binary",
     });
